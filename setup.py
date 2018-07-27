a65a44725b7615c92063f3870fc1eeeae6180230
Dom Corvasce <dom.corvasce@yandex.com>
2018-07-27 18:04:43 +0200

    Add initial setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='LUMI',
+    version='0.1.0',
+    description='',
+    author='Danilo Spinella, Luca Di Vita, Domenico Corvasce',
+    author_email='danyspin97@protonmail.com, d.v.luca99@gmail.com, dom.corvasce@yandex.com',
+    url='https://www.liveunix.org'
+)

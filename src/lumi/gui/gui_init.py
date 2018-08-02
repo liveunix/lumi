cf9b7632de3c7af9caaeaaa479e93eb4f50474fe
lucaoflaif <d.v.luca99@gmail.com>
2018-08-02 15:10:06 +0200

    create init module for gui initialization

---
+++ src/lumi/gui/gui_init.py
@@ -0,0 +1,17 @@
+import sys
+
+from PyQt5 import QtWidgets
+from gui_core import Ui_Dialog
+
+class ApplicationWindow(QtWidgets.QWidget):
+    def __init__(self):
+        super(ApplicationWindow, self).__init__()
+
+        self.ui = Ui_Dialog()
+        self.ui.setupUi(self)
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    application = ApplicationWindow()
+    application.show()
+    sys.exit(app.exec_())

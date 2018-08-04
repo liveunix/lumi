c8a79e9ea8c770ac0fbaf6e0519f1ea144959c2d
Dom Corvasce <dom.corvasce@yandex.com>
2018-08-04 19:00:29 +0200

    Add tests for repositories.py

--- src/lumi/linux/repositories.py
+++ src/lumi/linux/repositories.py
@@ -1,24 +1,27 @@
-import sh
 from pathlib import Path
+from sh.contrib import git
 from xdg.BaseDirectory import xdg_data_home
 
-def sync():
-    base_dir = xdg_data_home + '/lumi/'
-    grub_dir = base_dir + 'grub'
-    distro_dir = base_dir + 'distro'
+BASE_DIR = xdg_data_home + '/lumi/'
+DISTRO_REPO_URL = 'https://github.com/liveunix/lumi-grub.git'
+GRUB_REPO_URL = 'https://github.com/liveunix/lumi-grub.git'
 
-    # Check for the git repo
-    if not Path(grub_dir).exists():
-        # Clone if it does not exists
-        sh.contrib.git.clone('https://github.com/liveunix/lumi-grub.git', grub_dir)
-    else:
-        # Pull update if it exists
-        sh.git.pull('origin', 'master', rebase=True, _cwd=grub_dir)
+def sync_repo(uri, dir):
+    """Synchronize a single repository"""
+    repo_exists = Path(dir).exists()
 
-    # Same for distro data
-    if not Path(distro_dir).exists():
-        sh.git.clone('https://github.com/liveunix/liveunix-data', distro_dir)
-    else:
-        sh.git.pull('origin', 'master', rebase=True, _cwd=distro_dir)
+    if repo_exists is False:
+        return git.clone(uri, dir)
+    git.pull('origin', 'master', rebase=True, _cwd=dir)
 
+def sync(base_dir=BASE_DIR):
+    """Synchronize the LUMI repositories.
 
+    If the repositories are not on the system, clone them.
+    Otherwise, it pulls the updates from the remote repositories.
+    """
+    GRUB_DIR = base_dir + 'grub'
+    DISTRO_DIR = base_dir + 'distro'
+
+    sync_repo(GRUB_REPO_URL, GRUB_DIR)
+    sync_repo(DISTRO_REPO_URL, DISTRO_DIR)

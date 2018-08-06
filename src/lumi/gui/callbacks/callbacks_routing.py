b281699b99b8c2236ab66fa6f2f09639b8aec003
lucaoflaif <d.v.luca99@gmail.com>
2018-08-07 01:59:26 +0200

    add routing file

---
+++ src/lumi/gui/callbacks/callbacks_routing.py
@@ -0,0 +1,6 @@
+ROUTING = {
+        'install_available_distros_pushButton': {
+            'trigger_event': 'clicked',
+            'callback_functions': ['callbacks.printok_callback.print_ok',]
+        }
+    }

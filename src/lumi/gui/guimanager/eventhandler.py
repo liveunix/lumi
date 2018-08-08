ea387a5f80df4a14c56802d6c644f08739838b01
lucaoflaif <d.v.luca99@gmail.com>
2018-08-08 22:20:46 +0200

    eventhandler support multiple callbacks for single object event

--- src/lumi/gui/guimanager/eventhandler.py
+++ src/lumi/gui/guimanager/eventhandler.py
@@ -1,7 +1,7 @@
+from functools import partial
+
 from callbacks.callbacks_routing import ROUTING
 from PyQt5.QtCore import pyqtSignal as EVENT
-
-from callbacks.printok_callback import print_ok
 
 from importlib import import_module 
 
@@ -16,18 +16,18 @@ class GUIEventHandler():
     def get_module_object_by_path(self, module_path):
         return import_module(module_path)
     
-    def get_callback_function_by_module_and_name(self, module, function_name):
+    def get_callback_class_by_module_and_name(self, module, function_name):
         return getattr(module, function_name)
 
     def split_path_into_module_and_function(self, callback_path):
-        """@return : a dict with 'module' and 'function_name' indexes.
-           example: callbacks.example_callbacks.first_callback_function
+        """@return : a dict with 'module' and 'class_name' indexes.
+           example: callbacks.example_callbacks.callback_class
                     {'module': callbacks.example_callbacks,
-                    'function_name': first_callback_function}
+                    'class_name': callback_class}
         """
         return {
             'module': '.'.join(callback_path.split('.')[:-1]),
-            'function_name': callback_path.split('.')[-1]
+            'class_name': callback_path.split('.')[-1]
         }
 
 
@@ -39,8 +39,9 @@ class GUIEventHandler():
             gui_object = self.get_gui_object_by_name(object_name)
             event = self.get_object_event_attribute_by_name(gui_object, trigger_event)
 
-            path_info = self.split_path_into_module_and_function(callbacks_paths_list[0])
-            module = self.get_module_object_by_path(path_info['module'])
-            callback_function = self.get_callback_function_by_module_and_name(module, path_info['function_name'])
-            
-            event.connect(callback_function)
+            for callback_class_path in callbacks_paths_list:
+                path_info = self.split_path_into_module_and_function(callback_class_path)
+                module = self.get_module_object_by_path(path_info['module'])
+                callback_class = self.get_callback_class_by_module_and_name(module, path_info['class_name'])
+
+                event.connect(partial(callback_class.init(self)))

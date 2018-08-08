0cc14115649e70b87c3cf438a18b08f96208c95d
lucaoflaif <d.v.luca99@gmail.com>
2018-08-08 22:21:15 +0200

    add example callback's routing

--- src/lumi/gui/callbacks/callbacks_routing.py
+++ src/lumi/gui/callbacks/callbacks_routing.py
@@ -1,6 +1,7 @@
 ROUTING = {
         'install_available_distros_pushButton': {
             'trigger_event': 'clicked',
-            'callback_functions': ['callbacks.printok_callback.print_ok',]
+            'callback_functions': ['callbacks.printok_callback.print_ok',
+                                   'callbacks.print_itworks_callback.it_works']
         }
     }

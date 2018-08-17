a2d46a6fdaf8bd9b2b50ad4275ec739c5e3f3ba7
lucaoflaif <d.v.luca99@gmail.com>
2018-08-17 14:43:27 +0200

    add test callbacks

---
+++ src/lumi/gui/callbacks/test_distro_description.py
@@ -0,0 +1,15 @@
+
+class change_description:
+    def __init__(self, application):
+        self.application = application
+
+    @classmethod
+    def init(cls, application):
+        istance = cls(application)
+    
+        return istance.main
+    
+    def main(self):
+        print(self.application.sender().currentItem().text())
+        self.application.distro_description_textBrowser.setText('TEXT')
+        

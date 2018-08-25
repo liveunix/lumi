ffedf2369d2ceb629eaf6ac8de27bfa749b2e204
lucaoflaif <d.v.luca99@gmail.com>
2018-08-25 20:06:44 +0200

    add test application window

---
+++ tests/gui/application_window_class_test.py
@@ -0,0 +1,9 @@
+from lumi.gui.guimanager.init import ApplicationWindow
+
+class TestApplicationWindow(ApplicationWindow):
+
+    def show(self):
+        """Main ApplicationWindow class will call the show() method to render the window,
+           this is the test version of the ApplicationWindow class so we don't need to show the GUI:
+           overriding this method the rendering will be avoided."""
+        pass

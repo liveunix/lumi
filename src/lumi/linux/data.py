6b48b5b239541adef0527692682e15ca41f8891e
Danilo Spinella <danyspin97@protonmail.com>
2018-09-24 22:41:15 +0200

    Add verbose for sync methods in linux

--- src/lumi/linux/data.py
+++ src/lumi/linux/data.py
@@ -4,6 +4,7 @@ from lumi.core.statushandler import *
 from lumi.core.buffer import g_buf
 from pathlib import Path
 from sh.contrib import git
+from io import StringIO
 from xdg.BaseDirectory import xdg_data_home
 
 BASE_DIR = xdg_data_home + '/lumi/'
@@ -11,21 +12,25 @@ DISTRO_REPO_URL = 'https://github.com/liveunix/liveunix-data.git'
 GRUB_REPO_URL = 'https://github.com/liveunix/lumi-grub.git'
 
 
-def sync_repo(uri, dir, buf):
+def sync_repo(uri, dir, verbose):
     """Synchronize a single repository"""
     repo_exists = Path(dir).exists()
 
+    if verbose:
+        out_buf = g_buf()
+    else:
+        out_buf = StringIO()
+
     if repo_exists is False:
         g_buf().write("Cloning the repository {}.\n".format(uri))
-        g_buf().flush()
-        return git.clone(uri, dir, _out=buf)
+        return git.clone(uri, dir, _out=out_buf)
 
     g_buf().write("Updating the repository {} to the latest commit.\n".format(uri))
     g_buf().flush()
-    return git.pull('origin', 'master', _cwd=dir, _out=buf)
+    return git.pull('origin', 'master', _cwd=dir, _out=out_buf)
 
 
-def sync(base_dir=BASE_DIR):
+def sync(base_dir=BASE_DIR, verbose=False):
     """Synchronize the LUMI repositories.
 
     If the repositories are not on the system, clone them.
@@ -34,9 +39,9 @@ def sync(base_dir=BASE_DIR):
     GRUB_DIR = base_dir + 'grub'
     DISTRO_DIR = base_dir + 'distro'
 
-    sync_repo(GRUB_REPO_URL, GRUB_DIR, g_buf())
+    sync_repo(GRUB_REPO_URL, GRUB_DIR, verbose)
 
-    sync_repo(DISTRO_REPO_URL, DISTRO_DIR, g_buf())
+    sync_repo(DISTRO_REPO_URL, DISTRO_DIR, verbose)
 
 
 def get_distro_info(name_id='', base_dir=BASE_DIR):

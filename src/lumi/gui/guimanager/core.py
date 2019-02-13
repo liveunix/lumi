42cd829c2cd5e64df2df5db850e7801f146f99d2
lucaoflaif <d.v.luca99@gmail.com>
2019-02-13 22:42:37 +0100

    inherit QApplication as a QMainWindow's central widget

--- src/lumi/gui/guimanager/core.py
+++ src/lumi/gui/guimanager/core.py
@@ -1,6 +1,7 @@
 import sys
 
 from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QMainWindow
 from lumi.gui.guimanager.ui import Ui_Dialog
 from lumi.gui.guimanager.eventhandler import GUIEventHandler
 from lumi.gui.callbacks.callbacks_routing import ROUTING_OPTIONS
@@ -10,14 +11,16 @@ from lumi.gui.callbacks.callbacks_routing import ROUTING_OPTIONS
 # from lumi.gui.guimanager.statushandler import GUIStatusHandler
 
 
-class ApplicationWindow(QtWidgets.QWidget, Ui_Dialog, GUIEventHandler):
+class ApplicationWindow(QMainWindow, QtWidgets.QWidget, Ui_Dialog, GUIEventHandler):
     def _construct_qapplication(self):
         self.app = QtWidgets.QApplication(sys.argv)
+        pass
 
     def _setup_ui(self):
         self.setup_ui_dialog()
         for index, tabname in enumerate(["tab", "tabddd"]):
             self.setup_tab_structure_with_index_and_name(index, tabname)
+
         self.set_buddies()
         # self.retranslate_ui_dialog()
 
@@ -31,8 +34,22 @@ class ApplicationWindow(QtWidgets.QWidget, Ui_Dialog, GUIEventHandler):
         self._construct_qapplication()
         super(ApplicationWindow, self).__init__()
 
+        
         self._setup_ui()
-        # self._setup_gui_status_handler()
+
+        menubar = self.menuBar()
+        menu = menubar.addMenu('File')
+        db_action = menu.addAction("Open file")
+        db_action.setStatusTip("Select a file to use as a database")
+        #db_action.triggered.connect(self.open_new_db)
+        menubar.addMenu(menu)
+
+        self.statusBar().showMessage("Ready")
+        self.setCentralWidget(self.tabWidget)
+
+        #self.w.setCentralWidget(self.tabWidget)
+        #self.menu = QtWidgets.QMenuBar()
+        #self._setup_gui_status_handler()
 
         self.route_callbacks_to_objects(ROUTING_OPTIONS)
 

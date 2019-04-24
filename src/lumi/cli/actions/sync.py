84783f1674348fdce8ec991edb217de5cfef0502
Danilo Spinella <danyspin97@protonmail.com>
2019-04-24 14:23:13 +0000

    cli: Remove subcommands and options

--- src/lumi/cli/actions/sync.py
+++ src/lumi/cli/actions/sync.py
@@ -1,7 +1,4 @@
 from lumi.linux import data
-import lumi.core.buffer
-import sys
-from io import StringIO
 
 
 def dispatch(action):
--- src/lumi/cli/parser.py
+++ src/lumi/cli/parser.py
@@ -4,4 +4,20 @@ def get_parser():
     parser = ArgumentParser(description="Lumi CLI")
     subparsers = parser.add_subparsers(dest="action")
 
+    add = subparsers.add_parser("add", description="Add a distro/stage to a device", help="add <device> <distro>")
+
+    delete = subparsers.add_parser("delete", description="Delete a distro/stage from a device", help="delete <device> <distro>")
+
+    install = subparsers.add_parser("install", description="Install Lumi on a device")
+
+    list_p = subparsers.add_parser("list", )
+
+    list_all = subparsers.add_parser("list-all")
+
+    show = subparsers.add_parser("show-device")
+
+    show = subparsers.add_parser("show-system")
+
+    sync = subparsers.add_parser("sync")
+
     return parser
